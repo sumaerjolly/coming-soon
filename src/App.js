8fadf54266aba9c5a5237f875fbc2f2b871842a9
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-25 19:16:49 +0530

    Project Ready to start

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div>
       <h1>Upcoming Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati illo
+        nesciunt aperiam maxime alias sapiente nihil mollitia sunt, cum quis.
+      </p>
     </div>
   );
 }
