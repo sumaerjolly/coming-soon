26992e9e6b5fbe020bade2ccab3784153197a907
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-27 16:57:51 +0530

    add setCountdown function and useEffect for component will mount and unmount

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,15 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment, { duration } from 'moment';
 
-function Countdown() {
+function Countdown(props) {
   const initState = {
     days: 0,
     hours: 0,
-    mins: 0,
-    secs: 0
+    minutess: 0,
+    secondss: 0
   };
+
   const [state, setState] = useState(initState);
-  return <div></div>;
+
+  const setCountdown = () => {
+    const futureDate = moment(props.futureDate);
+    const today = moment();
+    const clockDuration = duration(futureDate.diff(today));
+    const days = Math.floor(clockDuration.asDays());
+    const hours = clockDuration.hours();
+    const minutes = clockDuration.minutes();
+    const seconds = clockDuration.seconds();
+    setState({ days, hours, minutes, seconds });
+  };
+
+  // set interval when component mounts and clean up
+  useEffect(() => {
+    setCountdown();
+    const interval = setInterval(() => {
+      setCountdown();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="countdown">
+      {Object.keys(state).map((key, i) => {
+        <div className="countdown-segment">
+          <span className="countdown-segment-number">{state.key}</span>
+          <span className="countdown-segment-caption">{key.toUpperCase()}</span>
+        </div>;
+      })}
+    </div>
+  );
 }
 
 export default Countdown;
